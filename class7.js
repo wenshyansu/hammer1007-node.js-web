cd5b9e5e41ca6dd05ccb3a9301a46a1af5ec21a9
Rock <wenshyansu@yahoo.com.tw>
2019-11-17 16:57:54 +0800

    add class7.js

---
+++ class7.js
@@ -0,0 +1,13 @@
+/*
+node.js從無到有建立網站2-4：顯示網頁流程
+http://hammer1007.blogspot.com/2017/11/nodejs2-4.html
+
+-. <public> 資料夾：放置公用的靜態檔案，預設資料夾：
+    <images>        // 圖片
+    <javascripts>   // javascript 程式
+    <stylesheets>   // CSS 檔案
+-. <routes> 資料夾：放置 controller 檔案 (node.js 檔案)
+-. <views> 資料夾：放置 view 檔案 (ejs模板檔案)
+
+-. app.js：網站設定檔
+ */
