a59be4db12f5f467808d13cbd481a1736b1dd0e7
Rock <wenshyansu@yahoo.com.tw>
2019-11-17 16:45:16 +0800

    add class6.js

---
+++ class6.js
@@ -0,0 +1,9 @@
+/*
+node.js從無到有建立網站2-3：建立基本網頁架構專案
+http://hammer1007.blogspot.com/2017/11/nodejs2-3.html
+
+<shell>
+express -e 專案名稱         // 建立以 ejs為模板引擎的 express 網頁專案
+npm install
+DEBUG=專案名稱:* & npm start
+ */
