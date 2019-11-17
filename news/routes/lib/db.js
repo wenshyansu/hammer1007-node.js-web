491c38437311e89c6113abffc6b73bc1d9ec4961
Rock <wenshyansu@yahoo.com.tw>
2019-11-17 21:42:16 +0800

    修正連線資訊與錯誤

--- news/routes/lib/db.js
+++ news/routes/lib/db.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql');
-
-var pool = mysql.createPool({
-    user : 'admin',
-    passwprd : 'admin',
-    host : 'localhost',
-    database : 'news',
-    waitForConnections : true,
-    connectionLimit : 10
+// 建立資料庫連線
+var pool  = mysql.createPool({
+    user: 'root',
+    password: '1qaz2wsx',
+    host: 'localhost',
+    database: 'news', 
+    waitForConnections : true, 
+    connectionLimit : 10       
 });
+
+module.exports = pool;
