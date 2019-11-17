bcc2ce8f85b93a324294108a44dd94e20ffc8301
Rock <wenshyansu@yahoo.com.tw>
2019-11-17 17:07:42 +0800

    調整 index 回傳資料

--- news/routes/index.js
+++ news/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { ntype: '公告', nsubject: '新聞測試！', ndate: '2017-11-01', nhits: '1' });
 });
 
 module.exports = router;
