2cd590157c0dfdb1f403854c69bb37326786d0d9
Rock <wenshyansu@yahoo.com.tw>
2019-11-17 16:23:03 +0800

    add class4.js

---
+++ class4.js
@@ -0,0 +1,19 @@
+/*
+node.js從無到有建立網站2-1：套件管理器npm
+http://hammer1007.blogspot.com/2017/11/nodejs2-1npm.html
+
+npm 套件管理
+<shell>
+
+// 安裝套件
+npm install 套件名稱
+npm install 套件名稱 -g     // 全域安裝
+
+// 查詢目前安裝套件
+npm ls
+npm ls -g                   // 全域查詢
+
+// 移除套件
+npm uninstall 套件名稱
+npm uninstall 套件名稱 -g   // 移除全域安裝套件
+ */
