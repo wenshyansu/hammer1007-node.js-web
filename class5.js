f0c3ff1b9e5aa97b7b802f7f16bb2e43d286337e
Rock <wenshyansu@yahoo.com.tw>
2019-11-17 16:26:33 +0800

    add class5.js

---
+++ class5.js
@@ -0,0 +1,12 @@
+/*
+node.js從無到有建立網站2-2：網頁框架
+http://hammer1007.blogspot.com/2017/11/nodejs2-2.html
+
+-. express 網頁框架
+預設的模板引擎是 jade
+目前最多人使用的模板引擎是 jade 及 ejs
+
+<shell>
+npm install express-generator -g
+
+ */
