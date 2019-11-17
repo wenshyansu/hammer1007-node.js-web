16935bd3d786c3c61b3a830382d42033344121b1
Rock <wenshyansu@yahoo.com.tw>
2019-11-18 01:18:15 +0800

    調整 index 加入資料庫

--- news/routes/index.js
+++ news/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var pool = require('./lib/db.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { ntype: '公告', nsubject: '新聞測試！', ndate: '2017-11-01', nhits: '1' });
+  pool.query('SELECT * FROM news.newscenter ORDER BY news_date DESC', function(err, results) {
+    if (err) throw err;
+    res.render('index', { data:results});
+
+    console.log(results[0]);
+  });
 });
 
 module.exports = router;
